f1c480660c81a9e713324bf9a6421d7a39b80451
Stefan Brankovic <79327006+stefcon@users.noreply.github.com>
2022-11-30 20:01:48 +0100

    Demo working
    Closing QR code closes app - needs fix

--- pos_app/lib/main.dart
+++ pos_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dateafterpay/qr_screen.dart';
 import 'package:dateafterpay/util.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -5,6 +6,17 @@ import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyBehaviour extends ScrollBehavior {
+  @override
+  ScrollPhysics getScrollPhysics(BuildContext context) =>
+      const ClampingScrollPhysics();
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -15,9 +27,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Date After Pay',
       theme: ThemeData(
+        brightness: Brightness.light,
         primarySwatch: Colors.pink,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        materialTapTargetSize: MaterialTapTargetSize.padded,
       ),
-      home: const MyHomePage(title: 'Date After Pay'),
+      builder: (context, child) {
+        return ScrollConfiguration(
+            behavior: MyBehaviour(),
+            child: const MyHomePage(title: 'Date After Pay'));
+      },
+      // home: const MyHomePage(title: 'Date After Pay'),
     );
   }
 }
@@ -32,66 +52,129 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String? scannedUserId;
+  bool qrOpen = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          SizedBox(height: paddy),
-          QrImage(
-            data: """
-{
-	products: [
-		{
-			title:	"Jack Daniels 0.7",
-			count: 1,
-		},
-		{
-			title:	"Jabuka Ajdared",
-			count: "1kg",
-		},
-		{
-			title:	"Cigarete Marlboro 100s",
-			count: 1,
-		},
-		{
-			title:	"Negro bombone",
-			count: 3,
-		},
-		{
-			title:	"Milka Noisette",
-			count: 2,
-		},
-	]
-}""",
-            version: QrVersions.auto,
-            size: 200.0,
-          ),
-          Expanded(
-            child: ListView(
-                shrinkWrap: true,
-                scrollDirection: Axis.vertical,
-                padding: EdgeInsets.all(paddy),
-                children: [
-                  _builtProductTile("1x Jack Daniels 0.7", 2500),
-                  _builtProductTile("1kg Jabuka Ajdared", 120),
-                  _builtProductTile("1x Cigarete Marlboro 100s", 470),
-                  _builtProductTile("3x Negro bombone", 360),
-                  _builtProductTile("2x Milka Noisette", 500),
-                ]),
-          )
-          // Container(
-          //   width: 50,
-          //   height: 50,
-          //   color: Colors.red,
-          // )
-        ],
-      ),
+      body: qrOpen
+          ? QRScanner(getScanned: (String scanned) {
+              setState(() {
+                scannedUserId = scanned;
+                qrOpen = false;
+              });
+            })
+          : Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: paddy),
+                scannedUserId == null
+                    ? InkWell(
+                        radius: 1001,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(1001)),
+                        splashColor: Colors.white38,
+                        onTap: () {
+                          setState(() {
+                            qrOpen = true;
+                          });
+                          // print("HEHEHEHE");
+                          // showModalBottomSheet(
+                          //     context: context,
+                          //     isScrollControlled: false,
+                          //     backgroundColor: Colors.transparent,
+                          //     builder: (ctx) {
+                          //       return Container(
+                          //         padding: EdgeInsets.only(
+                          //             bottom:
+                          //                 MediaQuery.of(ctx).viewInsets.bottom),
+                          //         child: QRScanner(getScanned: (String scanned) {
+                          //           setState(() {
+                          //             scannedUserId = scanned;
+                          //           });
+                          //         }),
+                          //       );
+                          //     });
+                        },
+                        child: Ink(
+                          width: MediaQuery.of(context).size.width * 0.6,
+                          height: MediaQuery.of(context).size.width * 0.6,
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).primaryColor,
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(1001)),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.black26,
+                                    spreadRadius: 5,
+                                    blurRadius: 10)
+                              ]),
+                          child: Container(
+                            child: Center(
+                              child: Text(
+                                "SCAN QR",
+                                style: TextStyle(
+                                    fontSize: 32,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      )
+                    : Column(
+                        children: [
+                          Text(scannedUserId!),
+                          SizedBox(height: paddy),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.4,
+                            height: MediaQuery.of(context).size.width * 0.4,
+                            decoration: BoxDecoration(
+                                border: Border.all(color: Colors.black, width: 1),
+                                borderRadius: const BorderRadius.all(
+                                    Radius.circular(1001)),
+                                ),
+                            child: Container(
+                              child: Center(child: Icon(Icons.check)),
+                            ),
+                          ),
+                          SizedBox(height: paddy),
+                          ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                scannedUserId = null;
+                              });
+                            },
+                            child: Text("U REDU"),
+                          )
+                        ],
+                      ),
+                SizedBox(height: paddy),
+                Expanded(
+                  child: ListView(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      padding: EdgeInsets.all(paddy),
+                      children: [
+                        _builtProductTile("1x Jack Daniels 0.7", 2500),
+                        _builtProductTile("1kg Jabuka Ajdared", 120),
+                        _builtProductTile("1x Cigarete Marlboro 100s", 470),
+                        _builtProductTile("3x Negro bombone", 360),
+                        _builtProductTile("2x Milka Noisette", 500),
+                      ]),
+                )
+                // Container(
+                //   width: 50,
+                //   height: 50,
+                //   color: Colors.red,
+                // )
+              ],
+            ),
       // floatingActionButton: FloatingActionButton(
       //   onPressed: _incrementCounter,
       //   tooltip: 'Increment',
